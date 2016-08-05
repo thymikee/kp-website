37577414c9adac18d363ececeb4538258f731302
Michał Pierzchała <thymikee@gmail.com>
2016-08-05 23:18:03 +0200

    Hero done, further sections WIP

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -1,16 +1,14 @@
 'use strict';
 
-var assetsTask = function (gulp, plugins, config, helpers) {
+var assetsTask = function (gulp, plugins, config) {
   gulp.task('assets', function () {
 
     plugins.del([config.dest.assets]);
 
-    var stream = gulp.src([config.src.assets, '!**/.keep'], { base: config.src.base })
+    return gulp.src([config.src.assets, '!**/.keep'], { base: config.src.base })
       .pipe(plugins.newer(config.dest.base))
       .pipe(gulp.dest(config.dest.base))
       .on('end', plugins.browserSync.reload);
-
-    return stream;
   });
 };
 
