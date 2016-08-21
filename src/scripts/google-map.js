8165cc7c4fa416f4e763046ee66c08a250df85b6
Michał Pierzchała <thymikee@gmail.com>
2016-08-21 12:34:39 +0200

    Map script naming refactoring

--- src/scripts/google-map.js
+++ src/scripts/google-map.js
@@ -10,7 +10,7 @@ class GoogleMap {
   constructor() {
     this.scrollListener = this.scrollListener.bind(this);
     this.center = { lat: 50.0171531, lng: 21.997 };
-    this.loadScript();
+    this.scheduleMapLoading();
   }
 
   initGoogleMap() {
@@ -69,7 +69,13 @@ class GoogleMap {
     });
   }
 
-  loadScript() {
+  scheduleMapLoading() {
+    this.handleResize();
+
+    window.addEventListener('scroll', this.scrollListener);
+  }
+
+  handleResize() {
     const mapTrigger = document.querySelector('.places__list');
     this.mapTriggerOffsetTop = mapTrigger.offsetTop;
     this.windowHeight = window.innerHeight;
@@ -78,8 +84,6 @@ class GoogleMap {
       this.mapTriggerOffsetTop = mapTrigger.offsetTop;
       this.windowHeight = window.innerHeight;
     });
-
-    window.addEventListener('scroll', this.scrollListener);
   }
 
   scrollListener() {
@@ -87,13 +91,17 @@ class GoogleMap {
 
     if (this.windowHeight + documentScrollTop > this.mapTriggerOffsetTop) {
       window.removeEventListener('scroll', this.scrollListener);
-      window.initMap = this.initGoogleMap.bind(this);
 
-      let script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBO-71BQKVYtvqPhnRlo3zZ05NUlp62sw4&callback=initMap';
-      document.body.appendChild(script);
+      this.loadMapScript();
     }
+  }
+
+  loadMapScript() {
+    window.initMap = this.initGoogleMap.bind(this);
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBO-71BQKVYtvqPhnRlo3zZ05NUlp62sw4&callback=initMap';
+    document.body.appendChild(script);
   }
 }
 
