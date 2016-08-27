feba5288a121a0a071333c37127a16bfdbf0dbda
Michał Pierzchała <thymikee@gmail.com>
2016-08-27 17:58:32 +0200

    Exclude CNAME from cleaning

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -5,7 +5,7 @@ var runSequence = require('run-sequence');
 var buildTask = function (gulp, plugins, config) {
 
   gulp.task('clean', function() {
-    return plugins.del([config.dest.base + '/**/*']);
+    return plugins.del([config.dest.base + '/**/*', '!' + config.dest.base + '/CNAME']);
   });
 
   gulp.task('build-critical', ['critical']);
