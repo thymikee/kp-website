87e5cff367acfd3a08de1db62beded30dda47654
Michał Pierzchała <thymikee@gmail.com>
2016-08-27 17:48:17 +0200

    Use parent deploy_key

--- deploy.sh
+++ deploy.sh
@@ -49,7 +49,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in kp_deploy_key.enc -out kp_deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../kp_deploy_key.enc -out kp_deploy_key -d
 
 chmod 600 kp_deploy_key
 eval `ssh-agent -s`
